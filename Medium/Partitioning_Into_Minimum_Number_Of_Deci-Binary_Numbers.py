9a74830e455f2f1b4b44549c7d049e85f7a3dc55
PrachponleuUch <ponleuvuthy@gmail.com>
2023-11-29 09:52:46 +1100

    Solved

--- Medium/Partitioning_Into_Minimum_Number_Of_Deci-Binary_Numbers.py
+++ Medium/Partitioning_Into_Minimum_Number_Of_Deci-Binary_Numbers.py
@@ -1,11 +1,6 @@
 class Solution(object):
     def minPartitions(self, n):
-        listn = list(n)
-        r = int(listn[0])
-        for i in range(len(listn) - 1):
-            if r < int(listn[i+1]):
-                r = int(listn[i+1])
-        return r
+        return int(max(n))
         """
         :type n: str
         :rtype: int
