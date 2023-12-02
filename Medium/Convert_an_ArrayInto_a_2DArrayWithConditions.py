e117e656f3475a12c71b1f22e7202a06aabc774b
PrachponleuUch <ponleuvuthy@gmail.com>
2023-12-02 16:16:31 +1100

    Solved

---
+++ Medium/Convert_an_ArrayInto_a_2DArrayWithConditions.py
@@ -0,0 +1,23 @@
+
+
+def findMatrix(nums):
+  matDict = {}
+  result = []
+  for num in nums:
+    matDict[num] = matDict.get(num, 0) + 1
+  
+  for num, amount in matDict.items():
+    while len(result) < amount:
+      result.append([])
+    while amount > 0:
+      result[amount-1].append(num)
+      amount -= 1
+    
+  return result
+
+  """
+  :type nums: List[int]
+  :rtype: List[List[int]]
+  """
+        
+findMatrix([1,3,4,1,2,3,1])
