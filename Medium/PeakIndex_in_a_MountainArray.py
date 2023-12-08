d5ab863a481236756aaf2a0174c141e8aff6aa37
PrachponleuUch <ponleuvuthy@gmail.com>
2023-12-08 16:44:46 +1100

    Solved

--- Medium/PeakIndex_in_a_MountainArray.py
+++ Medium/PeakIndex_in_a_MountainArray.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def peakIndexInMountainArray(self, arr):
-        for i in range (1, len(arr) - 1):
-            if (arr[i - 1] < arr[i]) and (arr[i] > arr[i + 1]):
-                return i
+        return arr.index(max(arr))
 
         """
         :type arr: List[int]
