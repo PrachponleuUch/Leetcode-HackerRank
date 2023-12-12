bae0dd9053ef677f97a10d4bfcb7227c08adc43e
PrachponleuUch <ponleuvuthy@gmail.com>
2023-12-12 17:59:21 +1100

    Solved

---
+++ Medium/minimized-maximum-of-products-distributed-to-any-store.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def minimizedMaximum(self, n, quantities):
+        def helper(mid):
+            count = 0
+            for q in quantities:
+              # If there's a remainder add 1, else 0 {True: 1, False: 0}
+                count += (q//mid) + (q%mid > 0)
+            return count <= n
+
+        low = 1
+        high = max(quantities)
+        while low <= high:
+            mid = (low + high) // 2
+            if helper(mid):
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
+        """
+        :type n: int
+        :type quantities: List[int]
+        :rtype: int
+        """
+        
