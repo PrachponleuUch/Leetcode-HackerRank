37cd9fcc06d905bc1d9eb3be7293403968a79dd4
PrachponleuUch <ponleuvuthy@gmail.com>
2024-02-02 15:57:54 +1100

    Solved

--- Medium/x.py
+++ Medium/x.py
@@ -1,16 +1,14 @@
 class Solution(object):
     def suggestedProducts(self, products, searchWord):
         ans = []
+        products.sort()
         for i in range(1, len(searchWord) + 1):
             block = []
             for product in products:
                 if searchWord[:i] in product[:i]:
                     block.append(product)
-            block.sort()
-            if len(block) > 3:
-                block = block[:3]
-            ans.append(block)
-        if not ans: ans = products.sort()
+            products = block
+            ans.append(block[:3])
         return ans
         """
         :type products: List[str]
