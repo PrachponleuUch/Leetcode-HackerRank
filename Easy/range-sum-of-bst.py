9882c3f6e15188d0b80ae818d874045264700568
PrachponleuUch <ponleuvuthy@gmail.com>
2024-02-21 17:41:37 +1100

    Feature: added info boxes on Home Page

---
+++ Easy/range-sum-of-bst.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        answer = 0
+
+        def helper(root, low, high):
+            nonlocal answer
+            if not root: return
+            if low <= root.val <= high:
+                answer += root.val
+            helper(root.left, low, high)
+            helper(root.right, low, high)
+        
+        helper(root, low, high)
+        return answer
+        
