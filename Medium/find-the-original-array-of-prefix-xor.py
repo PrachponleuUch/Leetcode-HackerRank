bd29654f7578ee48f4e4c0bb89901191b5763f56
PrachponleuUch <ponleuvuthy@gmail.com>
2024-06-06 21:25:38 +1000

    Retry

--- Medium/find-the-original-array-of-prefix-xor.py
+++ Medium/find-the-original-array-of-prefix-xor.py
@@ -1,10 +1,11 @@
 class Solution(object):
     def findArray(self, pref):
-        ans = [0] * len(pref)
-        ans[0] = pref[0]
+        arr = [None] * len(pref)
+        arr[0] = pref[0]
         for i in range(1, len(pref)):
-            ans[i] = pref[i]^pref[i-1]
-        return ans
+            arr[i] = pref[i - 1] ^ pref[i]
+        return arr
+
         """
         :type pref: List[int]
         :rtype: List[int]
