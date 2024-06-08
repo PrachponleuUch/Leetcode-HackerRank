9915de31d10b5dd72f2bfdf9ee4a17aaae2d05e4
PrachponleuUch <ponleuvuthy@gmail.com>
2024-06-08 17:11:50 +1000

    Solved

---
+++ Medium/queries-on-number-of-points-inside-a-circle.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def countPoints(self, points, queries):
+        cPoint = [0] * len(queries)
+        for index, query in enumerate(queries):
+            for point in points:
+                if (query[0] - point[0])**2 + (query[1] - point[1])**2 <= (query[2])**2:
+                    cPoint[index] += 1
+        return cPoint
+
+        
+        """
+        :type points: List[List[int]]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        
