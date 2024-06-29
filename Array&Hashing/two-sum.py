e113b6ac84b1742d1b9d541a317ac07aa319cf9f
PrachponleuUch <ponleuvuthy@gmail.com>
2024-06-29 19:48:05 +1000

    Optimized & Solved

--- Array&Hashing/two-sum.py
+++ Array&Hashing/two-sum.py
@@ -1,5 +1,18 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        hashMap = dict()
+        for idx, num in enumerate(nums):
+            if target - num in hashMap:
+                return [hashMap[target-num], idx]
+            hashMap[num] = idx
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+class Solution(object):
+    def twoSum(self, nums, target):
         for idx, num in enumerate(nums):
             for i in range(idx + 1, len(nums)):
                 if num + nums[i] == target:
